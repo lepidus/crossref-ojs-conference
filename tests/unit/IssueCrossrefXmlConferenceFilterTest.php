77c2d949926affb1f9fd63e0da4b4eaf6b4bbb4e
vitor <vitor@.lepidus.com.br>
2020-12-07 12:31:47 -0400

    Tentativa de importaçao das classes do ojs nos testes de unidade.
    Tarefa: documentacao-e-tarefas/desenvolvimento_e_infra#2
    
    Signed-off-by: vitor <vitor@lepidus.com.br>
    Signed-off-by: aline <aline@lepidus.com.br>

--- tests/unit/IssueCrossrefXmlConferenceFilterTest.php
+++ tests/unit/IssueCrossrefXmlConferenceFilterTest.php
@@ -1,4 +1,12 @@
 <?php declare(strict_types=1);
+
+require( '../ojs-3.2.1-1/tools/bootstrap.inc.php');
+
+
+//define('DIR','../ojs-3.2.1-1/lib/pkp/plugins/importexport/native/filter/');
+//include DIR . 'NativeExportFilter.inc.php';
+//include DIR . 'NativeImportExportFilter.inc.php';
+
 use PHPUnit\Framework\TestCase;
 
 class IssueCrossrefXmlConferenceFilterTest extends TestCase {
@@ -6,7 +14,9 @@ class IssueCrossrefXmlConferenceFilterTest extends TestCase {
     public function testCreateRootNode(){
 
 		$expectedDoiBatch =  new DOMDocument();
-        $expectedDoiBatch->load('tests/unit/conference-test.xml');
+		$expectedDoiBatch->load('../crossrefConference/tests/unit/conference-test.xml');
+
+		$filterGroup = new FilterGroup();
 
 		$doc = new DOMDocument('1.0', 'utf-8');
 		$crossRef = new IssueCrossrefXmlFilter();
