ada269a43eefd4032fcf8c15514d1b6bbc942ced
lucasanselmo <lucasanselmo@lepidus.com.br>
2023-07-20 16:52:49 -0300

    Fixe: Remove CROSSREF_CONFERENCE_XMLNS_JATS and CROSSREF_CONFERENCE_XMLNS_AI constants and references
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#537
    
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

--- filter/ProceedingsCrossrefXmlConferenceFilter.inc.php
+++ filter/ProceedingsCrossrefXmlConferenceFilter.inc.php
@@ -85,8 +85,6 @@ class ProceedingsCrossrefXmlConferenceFilter extends NativeExportFilter
         $deployment = $this->getDeployment();
         $rootNode = $doc->createElementNS($deployment->getNamespace(), $deployment->getRootElementName());
         $rootNode->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xsi', $deployment->getXmlSchemaInstance());
-        $rootNode->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:jats', $deployment->getJATSNamespace());
-        $rootNode->setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:ai', $deployment->getAINamespace());
         $rootNode->setAttribute('version', $deployment->getXmlSchemaVersion());
         $rootNode->setAttribute('xsi:schemaLocation', $deployment->getNamespace() . ' ' . $deployment->getSchemaFilename());
         return $rootNode;
